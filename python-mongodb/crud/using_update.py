25bd5c7d732021264f5df580044b2bdcadb375ea
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-06-10 19:12:50 -0300

    Include examples of update, replace and upsert

---
+++ python-mongodb/crud/using_update.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import pymongo
+import datetime
+
+# establish a connection to the database
+connection = pymongo.MongoClient("mongodb://localhost")
+
+
+# add a review date to a single record using update_one
+def add_review_date_using_update_one(student_id):
+
+    print("updating record using update_one and $set")
+    # get a handle to the school database
+    db = connection.school
+    scores = db.scores
+
+    try:
+        # get the doc
+        score = scores.find_one({'student_id': student_id, 'type': 'homework'})
+        print("before: ", score)
+
+        # update using set
+        record_id = score['_id']
+        result = scores.update_one({'_id': record_id},
+                          {'$set': {'review_date': datetime.datetime.utcnow()}})
+        print("num matched: ", result.matched_count)
+
+        score = scores.find_one({'_id': record_id})
+        print("after: ", score)
+
+    except Exception:
+        raise
+
+
+# add a review date to all records
+def add_review_dates_for_all():
+
+    print("updating record using update_many and $set")
+    # get a handle to the school database
+    db = connection.school
+    scores = db.scores
+
+    try:
+        # update all the docs
+        result = scores.update_many(
+            {}, {'$set': {'review_date': datetime.datetime.utcnow()}})
+        print("num matched: ", result.matched_count)
+
+    except Exception:
+        raise
+
+# removes all review dates
+def remove_all_review_dates():
+    print("\n\nremoving all review dates")
+
+    # get a handle to the school database
+    db=connection.school
+    scores = db.scores
+    try:
+        result = scores.update_many({'review_date':{'$exists':True}},
+                                    {'$unset':{'review_date':1}})
+        print("Matched this number of docs: ", result.matched_count)
+
+    except Exception as e:
+        print("Unexpected error:", type(e), e)
+        raise
+
+
+if __name__ == '__main__':
+    add_review_date_using_update_one(1)
+    #add_review_dates_for_all()
